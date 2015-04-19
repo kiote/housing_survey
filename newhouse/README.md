bdddd6e58327cd364bf655a9f3d2cecc6f1fcfef
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-04-19 16:49:04 +0700

    readme and new field added

---
+++ newhouse/README.md
@@ -0,0 +1,19 @@
+## Intro
+
+This project imports data from American Housing Survey (AHS) to MySQL database.
+
+## Import data
+
+to be able to import data, you should put data file into the
+
+```
+data/non-git
+```
+
+folder. For example, newhouse.csv should be putted into
+
+```
+data/non-git/puf2013/newhouse.csv
+```
+
+Since github can't hold such a big files, this manual step exists.
--- newhouse/models.py
+++ newhouse/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+import csv
 
 class Newhouse(models.Model):
-    control = models.BigIntegerField(db_column = "CONTROL", unique = True, primary_key = True)
+    control = models.BigIntegerField(db_column = 'CONTROL', unique = True, primary_key = True)
+    # Average heating/cooling degree days
+    degree = models.PositiveSmallIntegerField(db_column = 'DEGREE', null = True)
+
+    file_path = 'data/non-git/puf2013/newhouse.csv'
+
+    def readfile(self):
+        with open(self.file_path, 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
+            next(reader, None)  # skip the headers
+            for row in reader:
+                nh = Newhouse(control = int(row[0][1:-1]))
+                nh.degree = row[1]
+                nh.save()
+
+                print row[0]
