404131abb48ee5224ab6cd70f3078dd7f07984c0
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-04-19 17:31:13 +0700

    readme moved to root folder, new field added

--- newhouse/models.py
+++ newhouse/models.py
@@ -5,6 +5,8 @@ class Newhouse(models.Model):
     control = models.BigIntegerField(db_column = 'CONTROL', unique = True, primary_key = True)
     # Average heating/cooling degree days
     degree = models.PositiveSmallIntegerField(db_column = 'DEGREE', null = True)
+    # Central city / suburban status
+    metro3 = models.PositiveSmallIntegerField(db_column = 'METRO3', null = True)
 
     file_path = 'data/non-git/puf2013/newhouse.csv'
 
@@ -14,7 +16,8 @@ class Newhouse(models.Model):
             next(reader, None)  # skip the headers
             for row in reader:
                 nh = Newhouse(control = int(row[0][1:-1]))
-                nh.degree = row[1]
+                nh.degree = row[1][1:-1]
+                nh.metro3 = row[2][1:-1]
                 nh.save()
 
-                print row[0]
+                print row[2]
