6ae22d407086f8c70691c56864624574c6478cca
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-05-07 17:51:46 +0300

    remove Codebook processing

--- datatype/newhouse/models.py
+++ datatype/newhouse/models.py
@@ -1,7 +1,5 @@
 from datatype.abstract.models import AbstractDatatype
 from newhouse.models import Newhouse
-from datatype.codebook.models import Codebook
-import csv
 
 
 class NewhouseDatatype(AbstractDatatype):
@@ -11,18 +9,3 @@ class NewhouseDatatype(AbstractDatatype):
         Newhouse model should be already configured
         """
         AbstractDatatype('newhouse', self.sample).fill_model_by_csv_data(Newhouse)
-
-
-class Documenter:
-    file_path = 'data/columns/newhouse.csv'
-
-    def __init__(self):
-        c = Codebook()
-        with open('data/columns/documentor/newhouse.csv', 'wb') as csvfile:
-            docwriter = csv.writer(csvfile)
-
-            with open(self.file_path, 'rb') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
-
-                for row in reader:
-                    docwriter.writerow([row[0], c.get_info(row[0])])
