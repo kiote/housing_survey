8ef311be53173c54c10b208bb6eed820dab75cf0
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-05-12 16:09:42 +0300

    download both metro and national data

--- downloader/models.py
+++ downloader/models.py
@@ -5,24 +5,24 @@ import zipfile
 
 class Downloader:
     address = 'http://www2.census.gov/'
-    ahs_2013_name = 'AHS_2013_Metropolitan_PUF_v1.0_CSV.zip'
     ahs_2013_path = 'data/non-git/puf2013/'
-    files = {'file': {'remote': address + 'programs-surveys/ahs/2013/' + ahs_2013_name,
-                      'zip_file': ahs_2013_path + ahs_2013_name,
-                      'local_path': ahs_2013_path}}
 
-    def __init__(self):
-        self.files = self.files
+    ahs_2013_metro_name = 'AHS_2013_Metropolitan_PUF_v1.0_CSV.zip'
+    ahs_2013_metro_path = ahs_2013_path + 'metro/'
+    ahs_2013_national_name = 'AHS 2013 National PUF v1.1 CSV.zip'
+    ahs_2013_national_path = ahs_2013_path + 'national/'
+
+    files = [{'remote': address + 'programs-surveys/ahs/2013/' + ahs_2013_metro_name,
+              'zip_file': ahs_2013_path + ahs_2013_metro_name,
+              'local_path': ahs_2013_metro_path},
+             {'remote': address + 'programs-surveys/ahs/2013/' + ahs_2013_national_name,
+              'zip_file': ahs_2013_path + ahs_2013_national_name,
+              'local_path': ahs_2013_national_path}]
 
     def download(self):
-        try:
-            os.makedirs(self.ahs_2013_path)
-        except OSError as exc:
-            if exc.errno == errno.EEXIST and os.path.isdir(self.ahs_2013_path):
-                pass
-            else:
-                raise
-        for file_info in self.files.itervalues():
+        self.makedirs()
+
+        for file_info in self.files:
             if not os.path.isfile(file_info['zip_file']):
                 response = urllib2.urlopen(file_info['remote'])
                 result = response.read()
@@ -30,3 +30,13 @@ class Downloader:
                     result_file.write(result)
             with zipfile.ZipFile(file_info['zip_file']) as zf:
                 zf.extractall(file_info['local_path'])
+
+    def makedirs(self):
+        for mdir in [self.ahs_2013_metro_path, self.ahs_2013_national_path]:
+            try:
+                os.makedirs(mdir)
+            except OSError as exc:
+                if exc.errno == errno.EEXIST and os.path.isdir(mdir):
+                    pass
+                else:
+                    raise
