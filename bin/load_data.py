2a5bef294ce7009f47952b21a6cbe1baf17e0fc7
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-05-28 10:55:21 +0300

    fix truncate

--- bin/load_data.py
+++ bin/load_data.py
@@ -2,9 +2,10 @@ from django.db import connection
 import importlib
 
 files = ['homimp', 'mortg', 'newhouse', 'omov', 'owner', 'person', 'ratiov', 'repwgt', 'rmov', 'topical']
-truncate = "; ".join(["TRUNCATE `ahs_%s`;" % f for f in files])
+truncate = "; ".join(["TRUNCATE `ahs_%s`" % f for f in files])
+
 with connection.cursor() as c:
-    c.execute(truncate)
+    c.execute(truncate + ';')
 
 for _file in files:
     module = importlib.import_module("datatype.%s.models" % _file)
