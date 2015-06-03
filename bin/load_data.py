7a5671920962768ef84c09ae1f295a78dbfea23a
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-06-03 18:14:16 +0300

    change load_data to support two years

--- bin/load_data.py
+++ bin/load_data.py
@@ -6,25 +6,10 @@ truncate = "; ".join(["TRUNCATE `ahs_%s`" % f for f in files])
 with connection.cursor() as c:
     c.execute(truncate + ';')
 
-from datatype.homimp.models import HomimpDatatype
-from datatype.mortg.models import MortgDatatype
-from datatype.newhouse.models import NewhouseDatatype
-from datatype.omov.models import OmovDatatype
-from datatype.owner.models import OwnerDatatype
-from datatype.person.models import PersonDatatype
-from datatype.ratiov.models import RatiovDatatype
-from datatype.repwgt.models import RepwgtDatatype
-from datatype.rmov.models import RmovDatatype
-from datatype.topical.models import TopicalDatatype
+from datatype.saver.models import Datasaver
 
-HomimpDatatype().save_csv()
-MortgDatatype().save_csv()
-NewhouseDatatype().save_csv()
-OmovDatatype().save_csv()
-OwnerDatatype().save_csv()
-PersonDatatype().save_csv()
-RatiovDatatype().save_csv()
-RepwgtDatatype().save_csv()
-RmovDatatype().save_csv()
-TopicalDatatype().save_csv()
+years = [2013, 2011]
 
+for year in years:
+    for base_name in files:
+        Datasaver(year, base_name)
