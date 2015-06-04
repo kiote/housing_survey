529fa31f1a12a0f4592d01896e0be08bd05c58de
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-06-04 16:52:03 +0300

    more diffs + models changed according to diffs

--- homimp/models.py
+++ homimp/models.py
@@ -13,3 +13,4 @@ class Homimp(models.Model):
     ras = models.PositiveSmallIntegerField(db_column='RAS', null=True)
     jras = models.SmallIntegerField(db_column='JRAS', null=True)
     add_year = models.PositiveSmallIntegerField(db_column='ADD_YEAR', default=2013, db_index=True)
+    rahk = models.SmallIntegerField(db_column='RAHK', null=True)
