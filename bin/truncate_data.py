97a13f7ea8b380c8c52e606acff993ade241c4d0
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-06-17 19:33:50 +0300

    do no truncate non-existing table

--- bin/truncate_data.py
+++ bin/truncate_data.py
@@ -4,7 +4,7 @@ from django.db import connection
 
 files = ['ahs_homimp', 'ahs_mortg', 'ahs_newhouse', 'ahs_omov',
          'ahs_owner', 'ahs_person', 'ahs_ratiov', 'ahs_repwgt',
-         'ahs_rmov', 'ahs_topical', 'ahs_service_datatype']
+         'ahs_rmov', 'ahs_topical']
 truncate = "; ".join(["TRUNCATE `%s`" % f for f in files])
 
 with connection.cursor() as c:
