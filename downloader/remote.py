453b66959abccbcf94fc08ab8f76b9887eb3b5d4
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-07-15 08:51:16 +0200

    2003 year

--- downloader/remote.py
+++ downloader/remote.py
@@ -7,9 +7,7 @@ def year_attributes(year):
     file_name = 'National PUF v1.1 CSV.zip'
     if year == 2011:
         file_name = 'National and Metropolitan PUF v1.4 CSV.zip'
-    elif year == 2007:
-        file_name = 'National PUF V1.1 CSV.zip'
-    elif year == 2005:
+    elif year == 2007 or year == 2005 or year == 2003:
         file_name = 'National PUF V1.1 CSV.zip'
 
     quoted_file_name = urllib2.quote('AHS ' + str(year) + ' ' + file_name)
