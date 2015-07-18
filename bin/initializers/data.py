be9d559c21ce87d9986ed08017313a0c21c54be8
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-07-18 15:44:57 +0200

    1999 year added to data-file
      *.gen files for 1999 year also had been added

--- bin/initializers/data.py
+++ bin/initializers/data.py
@@ -1,6 +1,6 @@
 """Initialize data constants to be used in all bin/* scripts."""
 
 FILES = ['homimp', 'mortg', 'newhouse', 'omov', 'owner', 'person', 'ratiov',
-         'repwgt', 'rmov', 'topical']
+         'repwgt', 'rmov', 'topical', 'houshld']
 
-YEARS = [2013, 2011, 2009, 2007, 2005, 2003, 2001]
+YEARS = [2013, 2011, 2009, 2007, 2005, 2003, 2001, 1999]
