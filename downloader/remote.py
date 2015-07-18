1cb62b8c352b6d399e1b8e7299f1ed47e291e317
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-07-18 14:38:18 +0200

    Improve downloader

--- downloader/remote.py
+++ downloader/remote.py
@@ -4,13 +4,14 @@ import urllib2
 AHS_ADDRESS = 'http://www2.census.gov/'
 
 def year_attributes(year):
-    file_name = 'National PUF v1.1 CSV.zip'
     if year == 2011:
         file_name = 'National and Metropolitan PUF v1.4 CSV.zip'
-    elif year == 2007 or year == 2005 or year == 2003:
+    elif year in [2007, 2005, 2003, 1999]:
         file_name = 'National PUF V1.1 CSV.zip'
+    else #the only difference is downcased "v" here
+        file_name = 'National PUF v1.1 CSV.zip'
 
     quoted_file_name = urllib2.quote('AHS ' + str(year) + ' ' + file_name)
     remote_path = 'programs-surveys/ahs/' + str(year) + '/'
-    concatenated = AHS_ADDRESS + remote_path + quoted_file_name
-    return {'full_path': concatenated, 'file_name': quoted_file_name}
+    full_remote_path = AHS_ADDRESS + remote_path + quoted_file_name
+    return {'full_path': full_remote_path, 'file_name': quoted_file_name}
