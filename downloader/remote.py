8d8d02339722e859215f6e1b95e688fd416f4859
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-07-22 22:41:37 +0200

    Fix syntax error

--- downloader/remote.py
+++ downloader/remote.py
@@ -6,9 +6,9 @@ AHS_ADDRESS = 'http://www2.census.gov/'
 def year_attributes(year):
     if year == 2011:
         file_name = 'National and Metropolitan PUF v1.4 CSV.zip'
-    elif year in [2007, 2005, 2003, 1999]:
+    elif year in [2007, 2005, 2003, 1999, 1997]:
         file_name = 'National PUF V1.1 CSV.zip'
-    else #the only difference is downcased "v" here
+    else: #the only difference is downcased "v" here
         file_name = 'National PUF v1.1 CSV.zip'
 
     quoted_file_name = urllib2.quote('AHS ' + str(year) + ' ' + file_name)
