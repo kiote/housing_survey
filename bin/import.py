f9ac5fbf8e22144eb25b03b391b3be15447fa775
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-08-02 10:03:18 +0300

    downloader refactoring

--- bin/import.py
+++ bin/import.py
@@ -5,7 +5,14 @@ This script will download data files (csv) from AHS site and save it to
 data/non-git directory. Also it will unpack all files
 """
 import initializers.framework
+import downloader.remote as remote
+import downloader.local as local
+import initializers.data as di
 
 from downloader.models import Downloader
 
-Downloader().download()
+for y in di.YEARS:
+    remote_attrs = remote.year_attributes(y)
+    local_attrs = local.year_attributes(y)
+    full_path = local.FULL_PATH
+    Downloader(remote_attrs, local_attrs, full_path).download()
--- downloader/models.py
+++ downloader/models.py
@@ -1,29 +1,29 @@
 import urllib2
-import os, errno
+import os
+import errno
 import zipfile
-import remote
-import local
-
-import initializers.data as di
 
 """
 Downloads remote CSV-files from census.gov and uncompress them
 """
 class Downloader:
-    def get_file_list(self):
+    def __init__(self, remote_attrs, local_attrs, full_path):
+        self.remote_attrs = remote_attrs
+        self.local_attrs = local_attrs
+        self.full_path = full_path
+
+    def _get_file_params(self):
         files = []
-        for y in di.YEARS:
-            remote_attrs = remote.year_attributes(y)
-            local_attrs = local.year_attributes(y)
-            files.append({'remote': remote_attrs['full_path'],
-                          'zip_file': local_attrs['path'] + remote_attrs['file_name'],
-                          'local_path': local_attrs['csv_path']})
+        files.append({'remote': self.remote_attrs['full_path'],
+                      'zip_file': self.local_attrs['path'] + self.remote_attrs['file_name'],
+                      'local_path': self.local_attrs['csv_path']})
         return files
 
     def download(self):
+        """Download and unpack zip archive."""
         self._makedirs()
 
-        for file_info in self.get_file_list():
+        for file_info in self._get_file_params():
             print '---> Checking %s to exist locally' % file_info['zip_file']
             # do nothing if file exists already
             if os.path.isfile(file_info['zip_file']):
@@ -49,50 +49,45 @@ class Downloader:
         with zipfile.ZipFile(file_info['zip_file']) as zf:
             zf.extractall(file_info['local_path'])
 
-    @staticmethod
-    def _makedirs():
+    def _makedirs(self):
         """
-        Creates directories for data files, if this directories did not exist yet
-        """
-        csv_dirs_array = [local.year_attributes(y)['csv_path'] for y in di.YEARS]
-        for mdir in csv_dirs_array:
-            try:
-                os.makedirs(mdir)
-            except OSError as exc:
-                if exc.errno == errno.EEXIST and os.path.isdir(mdir):
-                    pass
-                else:
-                    raise
+        Create directories for data files.
 
-    @staticmethod
-    def _file_iterator():
-        csv_dirs_array = [local.year_attributes(y)['csv_path'] for y in di.YEARS]
-        for mdir in csv_dirs_array:
-            for mfile in os.listdir(local.FULL_PATH + '/' + mdir):
-                os.chdir(local.FULL_PATH + '/' + mdir)
-                yield mfile
-
-    @staticmethod
-    def _rename_files():
+        If this directories does not exist yet
         """
-        AHS give us CSV-files with different name for different years,
+        mdir = self.local_attrs['csv_path']
+
+        try:
+            os.makedirs(mdir)
+        except OSError as exc:
+            if exc.errno == errno.EEXIST and os.path.isdir(mdir):
+                pass
+            else:
+                raise
+
+    def _file_iterator(self):
+        mdir = self.local_attrs['csv_path']
+        for mfile in os.listdir(self.full_path + '/' + mdir):
+            os.chdir(self.full_path + '/' + mdir)
+            yield mfile
+
+    def _rename_files(self):
+        """
+        AHS give us CSV-files with different name for different years.
+
         for example newhouse.csv in 2011 named tnewhouse.csv
-
         So we just make names to follow the same pattern here
         """
-        for mfile in Downloader._file_iterator():
+        for mfile in self._file_iterator():
                 print '---> Renaming %s to %s' % (mfile, mfile.lower())
                 os.rename(mfile, mfile.lower())
                 if mfile.startswith("t") and mfile != 'topical.csv':
                     print '---> Renaming %s to %s' % (mfile, mfile[1:].lower())
                     os.rename(mfile, mfile[1:].lower())
 
-    @staticmethod
-    def _chunk_files():
-        """
-        Sometimes files are too big, so it's more comfortable to work with them when they are chunked
-        """
-        for mfile in Downloader._file_iterator():
+    def _chunk_files(self):
+        """Chunk big files."""
+        for mfile in self._file_iterator():
             if os.path.getsize(mfile) > 300000000:
                 print '---> Chunking big file %s' % mfile
                 os.system('split -l 30000 %s %s-segment-' % (mfile, mfile))
